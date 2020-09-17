0ff1a18b0651464f5ee402744963cd1a8055884e
gauravdwivedi <gauravdwivedi@hotmail.com>
2020-09-18 02:11:53 +0530

    scaffolding & setup

--- js-pacman-START-HERE/index.js
+++ js-pacman-START-HERE/index.js
@@ -1,1 +1,26 @@
-console.log('Working!');
+import { LEVEL, OBJECT_TYPE } from "./setup";
+
+//Dom elements
+
+const gameGrid = document.querySelector("#game");
+const scoreTable = document.querySelector("#score");
+const startButton = document.querySelector("#start-button");
+
+//Game Constants
+const POWER_PILL_TIME = 10000; //ms
+const GLOBAL_SPEED = 80; //ms
+
+//initial setup
+let score = 0;
+let timer = null;
+let gameWin = false;
+let powerPillActive = false;
+let powerPillTimer = null;
+
+function gameOver(pacman, grid) {}
+
+function checkCollision(pacman, ghosts) {}
+
+function gameLoop(pacman, ghosts) {}
+
+function startGame() {}
